041e6e76b2662b28b05b585c9d0437b80611769a
Andrea Ciliberti <meziu210@icloud.com>
2025-11-26 00:25:17 +0100

    Hello World con JavaFX

--- src/main/java/it/unisa/diem/softeng/easylibrary/Main.java
+++ src/main/java/it/unisa/diem/softeng/easylibrary/Main.java
@@ -1,12 +1,29 @@
 package it.unisa.diem.softeng.easylibrary;
 
-/**
- * Hello world!
- *
- */
-public class Main {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Lancia il runtime di JavaFX
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label lbl = new Label("Ciao Mondo!");
+        VBox root = new VBox();
+
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().addAll(lbl);
+
+        Scene scene = new Scene(root, 400, 300);
+        stage.setScene(scene);
+        stage.setTitle("EasyLibrary");
+        stage.show();
     }
 }
