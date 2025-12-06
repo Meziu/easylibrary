f20ca29cd68a02790068fdc154515a94e9d31a43
Serena Giannitti <serenagiannitti66@gmail.com>
2025-12-06 18:30:13 +0100

    Aggiunta eccezione per limite dei prestiti raggiunto

---
+++ src/main/java/it/unisa/diem/softeng/easylibrary/LimitePrestitiSuperatoException.java
@@ -0,0 +1,31 @@
+package it.unisa.diem.softeng.easylibrary;
+
+/**
+ *
+ * @brief Eccezione lanciata quando un utente tenta di registrare un nuovo prestito
+ * ma ha già raggiunto il limite massimo di prestiti attivi consentiti.
+ *
+ * Viene utilizzata per segnalare errori nella gestione dei prestiti, impedendo
+ * di superare il numero massimo di libri presi in prestito da un singolo utente.
+ * 
+ */
+public class LimitePrestitiSuperatoException extends RuntimeException {
+
+    /**
+     * @brief Costruttore di default.
+     *
+     * Crea una nuova eccezione senza specificare alcun messaggio di errore.
+     */
+    public LimitePrestitiSuperatoException() {
+    }
+    
+    /**
+     * @brief Costruttore con messaggio di errore.
+     *
+     * @param string messaggio descrittivo dell'errore relativo al limite dei prestiti attivi per quell'utente superato.
+     */
+    public LimitePrestitiSuperatoException(String string) {
+        super(string);
+    }
+    
+}
