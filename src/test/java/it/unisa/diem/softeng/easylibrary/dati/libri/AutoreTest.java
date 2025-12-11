12a235341099f8b2c72936b41edeb8a0185be5ab
Serena Giannitti <serenagiannitti66@gmail.com>
2025-12-11 15:35:45 +0100

    Aggiunta test classe Autore

--- src/test/java/it/unisa/diem/softeng/easylibrary/dati/libri/AutoreTest.java
+++ src/test/java/it/unisa/diem/softeng/easylibrary/dati/libri/AutoreTest.java
@@ -2,18 +2,28 @@ package it.unisa.diem.softeng.easylibrary.dati.libri;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 public class AutoreTest {
+    private Autore a;
 
     public AutoreTest() {
     }
 
+    @Before
+    public void setUp() {
+       a = new Autore("Mario", "Rossi");
+    }
+    @Test
+    public void testCostruzioneAutoreValida() {
+        assertNotNull(a);                                           // l'oggetto è creato
+        assertEquals("Mario", a.getAnagrafica().getNome());          // nome corretto
+        assertEquals("Rossi", a.getAnagrafica().getCognome());       // cognome corretto
+    }
+    
     @Test
     public void testGetAnagrafica() {
+        assertEquals(a.getAnagrafica().getNome(), "Mario");
+        assertEquals(a.getAnagrafica().getCognome(), "Rossi");
     }
-
-    @Test
-    public void testCompareTo() {
-    }
-
 }
