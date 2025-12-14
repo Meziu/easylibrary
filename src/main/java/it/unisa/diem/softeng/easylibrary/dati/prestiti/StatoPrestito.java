f43c8d5e1cd61f84087e77bf1ef57be4e2e6b635
Serena Giannitti <serenagiannitti66@gmail.com>
2025-12-14 13:57:04 +0100

    Modifica alla documentazione Doxygen di Stato Prestito

--- src/main/java/it/unisa/diem/softeng/easylibrary/dati/prestiti/StatoPrestito.java
+++ src/main/java/it/unisa/diem/softeng/easylibrary/dati/prestiti/StatoPrestito.java
@@ -3,10 +3,9 @@ package it.unisa.diem.softeng.easylibrary.dati.prestiti;
 import java.io.Serializable;
 
 /**
- *
  * @brief Stato del prestito.
- * Ogni \ref Prestito registrato è in uno stato tra "attivo", se il libro
- * associato non è stato ancora restituito dall'utente, e "restituito".
+ * Ogni \ref Prestito registrato è in uno stato tra ATTIVO, se il libro
+ * associato non è stato ancora restituito dall'utente, e RESTITUITO.
  */
 public enum StatoPrestito implements Serializable {
     /**
